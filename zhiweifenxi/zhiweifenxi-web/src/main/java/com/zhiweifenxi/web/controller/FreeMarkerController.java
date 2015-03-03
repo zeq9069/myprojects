b43224242efe3a4fadf7cc98e3cdaf800169a62f
zeq9069 <906928204@qq.com>
2015-03-03 09:28:56 +0800

    update freemarker

--- zhiweifenxi/zhiweifenxi-web/src/main/java/com/zhiweifenxi/web/controller/FreeMarkerController.java
+++ zhiweifenxi/zhiweifenxi-web/src/main/java/com/zhiweifenxi/web/controller/FreeMarkerController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
  
 /**
@@ -26,7 +27,20 @@ public class FreeMarkerController {
 		user.put("age", 32);
 		user.put("birthday", "1992-6");
 		map.put("user", user);
-		
 		return "test1.ftl";
 	}
+	 
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		@RequestMapping(value="test2")
+		public ModelAndView resolver3(ModelAndView model){
+			
+			Map user=new HashMap();
+			user.put("name", "kyrin");
+			user.put("age", 32);
+			user.put("birthday", "1992-6");
+			model.setViewName("test1.ftl");
+			model.addObject("user",user);
+			
+			return model;
+		}
 }
