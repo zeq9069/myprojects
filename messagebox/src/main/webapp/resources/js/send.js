108eb55cd4138854912d8a378651fd4262d3eea1
zeq9069 <906928204@qq.com>
2015-03-16 17:07:56 +0800

    add Ueditor

--- messagebox/src/main/webapp/resources/js/send.js
+++ messagebox/src/main/webapp/resources/js/send.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 	
 	$("#announ_submit").click(function(){
+		
+		
+		var value=UE.getEditor('editor').getContent();
+		 
+		alert($("form").serialize());
+		
 		$.post("/messagebox/system/announs/add",$("form").serialize(),function(data){
 			if(data.status=="success"){
 				alert("添加成功!");
